5961dc27632d899fcf51b205102aa4c09cb578fb
Vandeilltton <vandeilltton@gmail.com>
2020-02-14 13:52:44 -0300

    Revert "Update"
    This reverts commit d4915a95dc99e9fcc23511cc964e6d6e88c4486a.

--- Exercicio 2/atv16.php
+++
@@ -1,7 +0,0 @@
-<?php
-$i = 1;
-do{
-	$mult = $_GET["numero"]*$i;
-	echo $i." * ".$_GET["numero"]." = ".$mult."<br>";	
-	$i++;
-}while($i <= 10);
--- Exercicio 2/atv9.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$n = rand(0, 50);
-while($n != 37){
-	echo $n."<br>";
-	$n = rand(0, 50);
-}
-echo $n;
